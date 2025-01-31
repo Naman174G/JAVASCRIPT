7ddfc1f097d3c53d24f0ecdcaae8156cb6761824
Gurutav Patel <108912529+Naman174G@users.noreply.github.com>
2025-01-31 09:33:12 +0530

    Add files via upload

---
+++ list.jsx
@@ -0,0 +1,32 @@
+const App = () =>{
+
+  const numbers = [1, 2, 3, 4, 5];
+
+return(
+
+<main>
+ {numbers.map((number) => (
+   
+<ul key={number}>
+
+<li>{number}</li>
+
+</ul>
+
+
+
+
+))};
+
+
+
+</main>
+
+
+
+);
+
+
+
+
+};
